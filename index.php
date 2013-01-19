dfa32641c9deaf13020fd4ec284e20e02db9eb50
tianyi <douunasm@gmail.com>
2013-01-19 15:18:10 +0800

    testing

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <!--[if lt IE 7]><html lang="en" class="ie6"><![endif]--> 
 <!--[if IE 7]><html lang="en" class="ie7"><![endif]--> 
-<!--[if IE 8]><html lang="en" class="ie8"><![endif]--> 
 <!--[if IE 9]><html lang="en" class="ie9"><![endif]--> 
 <!--[if gt IE 9]><html lang="en"><![endif]-->
 <!--[if !IE]>--><html lang="en"><!--<![endif]-->
