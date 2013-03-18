75031d4cf14773284bff7dfb347c0f23d76c4fe9
unasm@sina.cn <https://unasm@sina.cn@github.com>
2013-03-18 09:58:11 +0800

    userspace2.php差不多了，应该多一个底部，恩，设计比上一个好了很多

--- ci/application/views/userSpace2.php
+++ ci/application/views/userSpace2.php
@@ -49,27 +49,30 @@
 -->
 <?php echo $this->load->view("m-spaceHeader2")?>
 <!-- 这里是最近动态，包括邮箱，图片，还有帖子,如果有动态，则显示，否则不显示，邮箱在前，帖子其次，其他看情况-->
-<div id="recent">
+<div id="recent" class = "divsion">
 	<div class="partTitle">	
 		<p >我的<span class="direc">帖子</span >动态</p>
 	</div>
 	<div  class = "clearfix">
 		<ul>
-			<li class="block clearfix">
+			<li class="partLi clearfix">
 				<img class="block liImg" title="田乙的世界" src="http://www.kukud.net/meinv/UploadFiles_3657/200907/20090723091813561.jpg"/>
+<!--
 				<div class="info">
+-->
 					<div class="detail">
 						<p>关于我的最新的动态,不过，或许你没有兴趣知道，不是吗</p>
 					</div>
 					<div class = "comInfo" >
 						<p>最新评价:<span class="author styleLine">田乙的世界田乙的世界</span><span class="part">电子商务</span></p>
-						<p>评价:3/浏览:6<span class="time">2012-2-3 4:2s:23</span></p>
+						<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
 					</div>
+<!--
 				</div>
+-->
 			</li>
-			<li class="block">
+			<li class="partLi clearfix">
 				<img class="block liImg" src="http://www.kukud.net/meinv/UploadFiles_3657/200907/20090723091813561.jpg"/>
-				<div class="info">
 					<div class="detail">
 					<p>关于我的最新的动态,不过，或许你没有兴趣知道，不是吗asdfkja=lksdfjlka kadjflk aslkdf akldsjflkds  aslkdfjlkasd s dflk sdfjlks  sdfkj  alksdjflaksjd kjskdlf sd k=sdf </p>
 					</div>
@@ -77,101 +80,124 @@
 						<p>楼主:</p>
 						<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
 					</div>
+			</li>
+			<li class="partLi clearfix">
+				<img class="block liImg" title="田乙的世界" src="http://www.kukud.net/meinv/UploadFiles_3657/200907/20090723091813561.jpg"/>
+					<div class="detail">
+						<p>关于我的最新的动态,不过，或许你没有兴趣知道，不是吗</p>
+					</div>
+					<div class = "comInfo" >
+						<p>最新评价:<span class="author styleLine">田乙的世界田乙的世界sadfasd阿斯发达岁的</span><span class="part">电子商务</span></p>
+						<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
+					</div>
+			</li>
+			<li class="partLi clearfix">
+				<img class="block liImg" title="田乙的世界" src="http://www.kukud.net/meinv/UploadFiles_3657/200907/20090723091813561.jpg"/>
+					<div class="detail">
+						<p>关于我的最新的动态,不过，或许你没有兴趣知道，不是吗</p>
+					</div>
+					<div class = "comInfo" >
+						<p>最新评价:<span class="author styleLine">田乙的世界田乙的世界</span><span class="part">电子商务</span></p>
+						<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
+					</div>
+			</li>
+			<li class="partLi clearfix">
+				<img class="block liImg" title="田乙的世界" src="http://www.kukud.net/meinv/UploadFiles_3657/200907/20090723091813561.jpg"/>
+<!--
+				<div class="info">
+-->
+					<div class="detail">
+						<p>关于我的最新的动态,不过，或许你没有兴趣知道，不是吗</p>
+					</div>
+					<div class = "comInfo" >
+						<p>最新评价:<span class="author styleLine">田乙的世界田乙的世界</span><span class="part">电子商务</span></p>
+						<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
+					</div>
+<!--
 				</div>
+-->
+			</li>
+			<li class="partLi clearfix">
+				<img class="block liImg" title="田乙的世界" src="http://www.kukud.net/meinv/UploadFiles_3657/200907/20090723091813561.jpg"/>
+<!--
+				<div class="info">
+-->
+					<div class="detail">
+						<p>关于我的最新的动态,不过，或许你没有兴趣知道，不是吗</p>
+					</div>
+					<div class = "comInfo" >
+						<p>最新评价:<span class="author styleLine">田乙的世界田乙的世界</span><span class="part">电子商务</span></p>
+						<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
+					</div>
+<!--
+				</div>
+-->
 			</li>
 		</ul>
 	</div>
 </div>
 <!--the end of the recent-->
 <!--这里显示的是空间主人的朋友的动态，按照value排序吧,没有顺序，随意排-->
-<div id="friend">
+<div id="friend" class = "divsion">
 	<div class="partTitle">
-		<p class="content">好友动态</p>
+		<p>好友动态</p>
 	</div>
-	<div class="content">
 		<ul>
-			<li class="block">
+			<li class="partLi">
 				<img class="block liImg" src="http://www.faxingw.cn/userimg/201208/462791.jpg"/>
 				<div class="info">
 					<div class="detail">
-					<p>关于c我的最新的动态,不过，或许你没有兴趣知道，不是吗</p>
-					</div>
+						<p>关于c我的最新的动态,不过，或许你没有兴趣知道，不是吗</p>
+				</div>
+				<div class = "comInfo">
 					<p>最新评价:</p>
 					<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
 				</div>
+				</div>
 			</li>
-			<li class="block">
+			<li class="partLi">
 				<img class="block liImg" src="http://static.panoramio.com/photos/original/36365461.jpg"/>
-				<div class="info">
-					<div class="detail">
+				<div class="detail">
 					<p>关于c我的最新的动态,不过，或许你没有兴趣知道，不是吗</p>
-					</div>
-					<p>最新评价:</p>
-					<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
+				</div>
+				<div class = "comInfo">
+						<p>最新评价:</p>
+						<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
 				</div>
 			</li>
-			<li class="block">
+			<li class="partLi">
 				<img class="block liImg" src="http://static.panoramio.com/photos/original/36365461.jpg"/>
-				<div class="info">
 					<div class="detail">
-					<p>关于c我的最新的动态,不过，或许你没有兴趣知道，不是吗</p>
+						<p>关于c我的最新的动态,不过，或许你没有兴趣知道，不是吗</p>
+					</div>		
+					<div class = "comInfo">
+						<p>最新评价:</p>
+						<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
 					</div>
-					<p>最新评价:</p>
-					<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
-				</div>
 			</li>
-			<li class="block">
-				<img class="block liImg" src="http://static.panoramio.com/photos/original/36365461.jpg"/>
-				<div class="info">
+			<li class="partLi">
+				<img class="block liImg" src="http://www.faxingw.cn/userimg/201208/462791.jpg"/>
 					<div class="detail">
-					<p>关于c我的最新的动态,不过，或许你没有兴趣知道，不是吗</p>
+						<p>关于c我的最新的动态,不过，或许你没有兴趣知道，不是吗</p>
 					</div>
-					<p>最新评价:</p>
-					<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
-				</div>
+					<div class = "comInfo">
+						<p>最新评价:</p>
+						<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
+					</div>
 			</li>
-			<li class="block">
+			<li class="partLi">
 				<img class="block liImg" src="http://www.faxingw.cn/userimg/201208/462791.jpg"/>
 				<div class="info">
 					<div class="detail">
 					<p>关于c我的最新的动态,不过，或许你没有兴趣知道，不是吗</p>
 					</div>
-					<p>最新评价:</p>
-					<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
-				</div>
-			</li>
-			<li class="block">
-				<img class="block liImg" src="http://www.faxingw.cn/userimg/201208/462791.jpg"/>
-				<div class="info">
-					<div class="detail">
-					<p>关于c我的最新的动态,不过，或许你没有兴趣知道，不是吗</p>
+					<div class = "comInfo">
+						<p>最新评价:</p>
+						<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
 					</div>
-					<p>最新评价:</p>
-					<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
-				</div>
-			</li>
-			<li class="block">
-				<img class="block liImg" src="http://www.faxingw.cn/userimg/201208/462791.jpg"/>
-				<div class="info">
-					<div class="detail">
-					<p>关于c我的最新的动态,不过，或许你没有兴趣知道，不是吗</p>
-					</div>
-					<p>最新评价:</p>
-					<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
-				</div>
-			</li>
-			<li class="block">
-				<img class="block liImg" src="http://www.faxingw.cn/userimg/201208/462791.jpg"/>
-				<div class="info">
-					<div class="detail">
-					<p>关于c我的最新的动态,不过，或许你没有兴趣知道，不是吗</p>
-					</div>
-					<p>最新评价:</p>
-					<p>评价:3/浏览:6<span class="time">2012-2-3</span></p>
 				</div>
 			</li>
 		</ul>
-	</div>
 </div>
 <!--the end of friend-->
 <div id="recommend">
