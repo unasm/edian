385b2d65cb75a6d9a36fe34d0d95722544241a32
unasm <unasm@sina.cn>
2013-07-24 22:57:39 +0800

    百度地图的加载

--- edian/application/views/item.php
+++ edian/application/views/item.php
@@ -58,13 +58,14 @@ var user_id="<?php echo trim($this->session->userdata('user_id'))?>";
         <div class="pdc">
             <!-- short for product 就是介绍商品内容的页面-->
             <ul class="pg clearfix" id = "pg">
-                <li name = "more">详情</li>
-                <li class = "cse" name = "judge">评价</li>
+                <li class = "cse" name = "more">详情</li>
+                <li name = "comment">评价</li>
             </ul>
             <div class="des" id = "des">
                 <!-- short for descript-->
+                <p>testing</p>
             </div>
-            <div class = "dcom" >
+            <div class = "dcom" id = "dcom" style = "display:none" >
                 <p class = "coms"><a>全部(23)</a><a name = "h">超赞(18)</a><a name = "m">还不错(2)</a><a>勉强(1)</a><a>暴走(2)</a></p>
                 <ul class = "com" id = "com">
                 <!-- short for comment-->
@@ -131,6 +132,27 @@ var user_id="<?php echo trim($this->session->userdata('user_id'))?>";
     </div>
     <script type="text/javascript" charset="utf-8" src = " <?php echo $baseUrl.'js/jquery.js' ?>"></script>
     <script type="text/javascript" charset="utf-8" src = " <?php echo $baseUrl.'js/item.js' ?>"></script>
+    <script type="text/javascript" charset="utf-8" src = "http://api.map.baidu.com/api?v=1.5&ak=672fb383152ac1625e0b49690797918d"></script>
+<!--
+    document.write('<script type="text/javascript" src="http://api.map.baidu.com/api?v=1.5&ak=672fb383152ac1625e0b49690797918d">\x3C/script>');
+</script>
+-->
+<script type="text/javascript" charset="utf-8">
+window.onload = mapInit;
+function mapInit(){
+    console.log("test");
+    var map = new BMap.Map("allmap");
+    map.enableScrollWheelZoom();                            //启用滚轮放大缩小
+    map.enableInertialDragging();
+    map.enablePinchToZoom();//双指缩放
+    map.enableAutoResize();
+    var lat = "30.757588",lny = "103.93707";//可以的话，就更大体定位吧,这种方式不好
+    var point = new BMap.Point(lny,lat);
+    map.centerAndZoom(point,17);//默认开始定位在科大附近
+}
+</script>
+-->
+<script type="text/javascript" charset="utf-8" id = "mapId"></script>
 </body>
 </html>
 
--- edian/js/item.js
+++ edian/js/item.js
@@ -5,21 +5,55 @@
     > Last_Modified: 2013-07-24 16:22:51
  ************************************************************************/
 $(document).ready(function(){
-    console.log("hello");
     pg();//集中了页面切换的操作
+    //$("#body").append('<script type="text/javascript" src="http://api.map.baidu.com/api?v=1.5&ak=672fb383152ac1625e0b49690797918d">\x3C/script>');
+    /*
+    $("#mapId").attr("src","
+                     http://api.map.baidu.com/api?v=1.5&ak=672fb383152ac1625e0b49690797918d"
+                    );
+    setTimeout(function() {
+        $("#mapId").onload = function(){
+        console.log("test");
+        var map = new BMap.Map("allmap");
+        map.enableScrollWheelZoom();                            //启用滚轮放大缩小
+        map.enableInertialDragging();
+        map.enablePinchToZoom();//双指缩放
+        map.enableAutoResize();
+        var lat = "30.757588",lny = "103.93707";//可以的话，就更大体定位吧,这种方式不好
+        var point = new BMap.Point(lny,lat);
+        map.centerAndZoom(point,17);//默认开始定位在科大附近
+    }
+
+    }, 1000);
+*/
 })
 function pg() {
     var temp,pg = $("#pg");//pg 页面切换的ul
     $("#judge").click(function () {
         var lis = pg.find("li");
         for (var i = lis.length - 1; i >= 0; i --) {
-            temp = $(lis[i]).class();//不知道所个class的时候，会不会出错呢
-            console.log(temp);
-            if(temp == "cse")$(temp[i]).removeClass("cse");
+            temp = $(lis[i]).attr("class");//不知道所个class的时候，会不会出错呢
+            if(temp == "cse")$(lis[i]).removeClass("cse");
             temp = $(lis[i]).attr("name");
-            if(temp == "judge"){
-                $(temp).addClass("cse");
+            if(temp == "comment"){
+                $(lis[i]).addClass("cse");
             }
         }
+    });
+    var des = $("#des"),dcom = $("#dcom");
+    var last = des;
+    pg.delegate("li","click",function(){
+        var name = $(this).attr("name");
+        pg.find(".cse").removeClass("cse");
+        $(this).addClass("cse");
+        last.fadeOut(function(){
+            if(name == "more"){
+                last = des;
+                last.fadeIn();
+            }else if(name == "comment"){
+                last = dcom;
+                last.fadeIn();
+            }
+        })
     })
 }
