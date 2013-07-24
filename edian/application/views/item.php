5dcf1641b1c49e708cbd1b357c0246a7c1bfec82
unasm <unasm@sina.cn>
2013-07-24 14:56:50 +0800

    item的css和html完成，显示输入后台信息和js内容

--- edian/application/views/item.php
+++ edian/application/views/item.php
@@ -32,17 +32,98 @@ var user_id="<?php echo trim($this->session->userdata('user_id'))?>";
                <li> <img src = "http://www.edian.cn/upload/191374150239.jpg" /></li>
             </ul>
         </div>
-        <div class = "det">
+        <div class = "det clearfix">
             <h3>
                 美好的明天还需要兄弟一起创造
             </h3>
             <p><span class = "item">价格:</span>￥<em class = "sp">12</em></p>
             <p><span class = "item">评分:</span>9.5</p>
             <p>
-                <span class="ht"><span class = "item">销量:</span> 23</span>
-                <span class="ht"><span class = "item">评价:</span> 15</span>
-                <span class="ht"><span class = 'item'>浏览:</span>230</span>
+                <span class="ht"><span class = "item">销量:</span> <span class = "sep">23</span></span>
+                <span class="ht"><span class = "item">评价:</span><span class = "sep"> <a id = "judge" href = "#tojudge" >15</a></span></span>
+                <span class="ht"><span class = 'item'>浏览:</span><span class = "sep">230</span></span>
             </p>
+            <p><span class = "item">营业起止时间:</span>8:00-- 19:00</p>
+            <p id = "num"><span class = "item">购买数量:</span><input type="text" name="buyNum" id="buyNum" value="1" /><span id = "storeNum">库存:234</span><button class="inc">+</button><button  class="dec">-</button></p>
+            <p><a class = "bton ba" href = "buy/itemId">立即购买</a><a class = "bton" href = "userId/itemId">加入购物车</a></p>
+            <p> <span class="item">承诺:</span>送货上门</p>
+        </div>
+         <div id="user" class = "user">
+            <div id="allmp">
+            </div>
+        </div>
+        <div class="pdc">
+            <!-- short for product 就是介绍商品内容的页面-->
+            <ul class="pg clearfix">
+                <li>详情</li>
+                <li class = "cse">评价</li>
+            </ul>
+            <div class="des" id = "des">
+                <!-- short for descript-->
+            </div>
+            <div class = "dcom" >
+                <p class = "coms"><a>全部(23)</a><a name = "h">超赞(18)</a><a name = "m">还不错(2)</a><a>勉强(1)</a><a>暴走(2)</a></p>
+                <ul class = "com" id = "com">
+                <!-- short for comment-->
+                    <li>
+                        <p>
+                            <span >评分:</span><span class = "sp">9</span>
+                            <span >田乙的世界</span>
+                            <span >2012-12-14</span>
+                        </p>
+                        <blockquote>
+                            东西不错，值得夸奖
+                            东西不错，值得夸奖
+                            东西不错，值得夸奖
+                            东西不错，值得夸奖
+                            东西不错，值得夸奖
+                            东西不错，值得夸奖
+                            东西不错，值得夸奖
+                            东西不错，值得夸奖
+                            东西不错，值得夸奖
+                            东西不错，值得夸奖
+                        </blockquote>
+                    </li>
+                    <li>
+                        <p>
+                            <span >评分:</span><span class = "sp">9</span>
+                            <span >田乙的世界</span>
+                            <span >2012-12-14</span>
+                        </p>
+                        <blockquote>
+                            东西不错，值得夸奖,东西不错，值得夸奖
+                        </blockquote>
+                    </li>
+                    <li>
+                        <p>
+                            <span >评分:</span><span class = "sp">3</span>
+                            <span >田乙的世界</span>
+                            <span >2012-12-14</span>
+                        </p>
+                        <blockquote>
+                            东西不错，值得夸奖,东西不错，值得夸奖
+                        </blockquote>
+                    </li>
+                    <li>
+                        <p>
+                            <span >评分:</span><span class = "sp">6</span>
+                            <span >田乙的世界</span>
+                            <span >2012-12-14</span>
+                        </p>
+                        <blockquote>
+                            东西不错，值得夸奖,东西不错，值得夸奖
+                        </blockquote>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class = "bot">
+            <a class = "top" href = "#top">顶部</a>
+            <ul id="order">
+                <p>订单</p>
+            </ul>
+        </div>
+        <div id="footer">
         </div>
     </div>
 </body>
